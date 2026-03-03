a922613d112bfabea9f005acf156801be7dd57d9
lucyinthesky2111 <lucy.kidner@outlook.com>
2026-03-03 21:06:44 +0000

    Add sales data to salees worksheet

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open_by_url("Phttps://docs.google.com/spreadsheets/d/14gPXibKIrnmd5vuxpfTKUFnph_YeH1m5hOisJQDhhTw/edit?pli=1&gid=1680754323#gid=1680754323")
+SHEET = GSPREAD_CLIENT.open_by_url("https://docs.google.com/spreadsheets/d/14gPXibKIrnmd5vuxpfTKUFnph_YeH1m5hOisJQDhhTw/edit?pli=1&gid=1680754323#gid=1680754323")
 
 def get_sales_data():
     """
@@ -49,7 +49,17 @@ def validate_data(values):
         return False
         
     return True
-    
 
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided.
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
     
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
